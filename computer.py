907435327f48cd9b82177c9306c1144da7a6eb53
Marchall Adams <50703766+MAdams1999@users.noreply.github.com>
2020-12-08 03:14:19 +0200

    Checks if the expression  is well formatted

--- computer.py
+++ computer.py
@@ -1,9 +1,23 @@
 import sys
 
+def checkExpression(expression):
+	reg = re.compile('[0-9X\s\^\-\+\*\=\.\/]+$')
+	const = re.search('(?<=[^X])\^', expression)
+	const2 = re.search('\^(?![0-9])', expression)
+	const3 = re.search('(?<=\d)\s(?=\d)', expression)
+
+	if not reg.match(expression):
+		print('Wrong characters in expression.')
+		return False
+	if const is not None or const2 is not None or const3 is not None:
+		print('Eh buddy the Polynome is not well formatted.')
+		return False
+	return True
+
 def printResult(result):
 
 	if result['solution'] is None:
-		print('Solution is all real numbers')
+		print('The Solution has only real numbers')
 
 	elif result['solution'] == False:
 		print('There is no answer to this expression')
@@ -21,3 +35,4 @@ def printResult(result):
 		else:
 			print('Polynomial degree: 2')
 			print('Discriminant is strictly positive, the two solutions are:\n{}\n{}'.format(result['solution1'], result['solution2']))
+
